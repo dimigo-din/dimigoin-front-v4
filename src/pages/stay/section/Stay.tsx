ada0ec4e0e37a6754189e7ac11873e0ee8c90811
velo1203 <hokitoki1203@gmail.com>
2025-06-16 22:25:00 +0900

    redirects

--- src/pages/stay/section/Stay.tsx
+++ src/pages/stay/section/Stay.tsx
@@ -10,26 +10,10 @@ const SeatSelect = styled.div`
 
     align-items: center;
     p {
+        padding: 0 4px;
         font-size: ${({ theme }) => theme.font.xl.size};
         color: ${({ theme }) => theme.colors.brand.primary};
         font-weight: ${({ theme }) => theme.font.xl.weight};
-    }
-`;
-
-const StayStatus = styled.p`
-    font-size: ${({ theme }) => theme.font.lg.size};
-    color: ${({ theme }) => theme.colors.font.secondary};
-
-    span {
-        color: ${({ theme }) => theme.colors.brand.primary};
-        font-weight: ${({ theme }) => theme.font.baseBold.weight};
-        border-bottom: 2px solid ${({ theme }) => theme.colors.brand.primary};
-        cursor: pointer;
-        transition: color 0.3s ease, border-color 0.3s ease;
-        &:hover {
-            color: ${({ theme }) => theme.colors.button.primaryHover};
-            border-color: ${({ theme }) => theme.colors.button.primaryHover};
-        }
     }
 `;
 
@@ -44,11 +28,6 @@ function StaySection() {
             </Section>
             <Section label="좌석 미선택 사유">
                 <Input placeholder="좌석 미선택 사유 입력" />
-            </Section>
-            <Section label="잔류 현황">
-                <StayStatus>
-                    <span>여기</span>에서 잔류 신청 현황을 확인하세요
-                </StayStatus>
             </Section>
 
             <Button>잔류 신청</Button>
