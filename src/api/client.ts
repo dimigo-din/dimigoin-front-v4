51fd08565a177252e6e741b3579006b4558cb726
yeonfish <yeonfish6040@gmail.com>
2025-08-04 01:30:24 +0900

    chore: domain change

--- src/api/client.ts
+++ src/api/client.ts
@@ -5,8 +5,7 @@ let instance: AxiosInstance;
 export function getInstance(): AxiosInstance {
   if (!instance) {
     instance = axios.create({
-      // baseURL: 'http://localhost:3000',
-      baseURL: 'https://api.dimigo.site',
+      baseURL: location.host === "dimigoin.io" ? 'https://api.dimigoin.io' : "http://localhost:3000",
       timeout: 5000,
       withCredentials: true,
     });
