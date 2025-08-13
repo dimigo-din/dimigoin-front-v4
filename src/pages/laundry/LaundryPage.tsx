795a0f285804c5d80ba45c4b226196fb2fa35154
yeonfish <yeonfish6040@gmail.com>
2025-08-13 17:54:49 +0900

    chore: indicator matching

--- src/pages/laundry/LaundryPage.tsx
+++ src/pages/laundry/LaundryPage.tsx
@@ -88,7 +88,7 @@ function LaundryPage() {
         setMachines(list);
 
         if (!currentMachine && list.length > 0) {
-          setCurrentMachine(list[0]);
+          setCurrentMachine(list.find((m) => m.type === currentType));
         }
 
         return getLaundryApplies();
