0db60155b5ca1e8d54c627e6f4819ee01a3a727b
yeonfish <yeonfish6040@gmail.com>
2025-08-13 16:40:26 +0900

    fix: workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,4 @@ import svgr from "vite-plugin-svgr";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), svgr()],
-    server: {
-        allowedHosts: ["10.4.0.6"],
-    }
 });
