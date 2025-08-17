8c2c8c715f3370b4135d6357a1a77110d7d4269c
vvcnyy <yangwoochan68@gmail.com>
2025-08-17 21:13:06 +0900

    fix: profile

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -80,8 +80,8 @@ function LoginPage() {
       googleLogin(code).then(({accessToken}) => {
         const payload = JSON.parse(atob(accessToken.split(".")[1]));
         localStorage.setItem("id", payload.id);
+        localStorage.setItem("picture", payload.picture);
         getPersonalInformation(prompt("개인정보를 등록할때 입력한 인증번호를 입력해주세요.")!).then((data) => {
-          console.log(data);
           localStorage.setItem("grade", data.grade.toString());
           localStorage.setItem("class", data.class.toString());
           localStorage.setItem("number", data.number.toString());
