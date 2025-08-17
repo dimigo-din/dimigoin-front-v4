168f4f197fa88a45134c5a83aac7c584607400a0
yeonfish <yeonfish6040@gmail.com>
2025-08-17 15:19:09 +0900

    fix: no delay to register

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -104,8 +104,8 @@ function LoginPage() {
         showToast(e.response.data.error, "danger");
 
         if (e.response.data.code === "PersonalInformation_NotRegistered") {
-          showToast("5초후에 개인정보 등록 페이지로 이동합니다.", "info")
-          setTimeout(() => location.href = "https://dimiauth.findflag.kr", 5000);
+          showToast("5초후에 개인정보 등록 페이지로 이동합니다.", "info");
+          location.href = "https://dimiauth.findflag.kr"
         }
       });
     }
